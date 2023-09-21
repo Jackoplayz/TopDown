4edbddd3b6a4a7b8e3aa087b47124fe5f81a1ed8
Jack <mariojack@live.com.au>
2023-09-21 13:50:17 +1000

    Commit started

---
+++ Assets/Player.cs
@@ -0,0 +1,77 @@
+
+using System;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class Player : MonoBehaviour
+
+{
+    public float speed;
+    public Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 newCameraPosition = new Vector3(transform.position.x, transform.position.y, -5);
+
+        Camera.main.transform.position = newCameraPosition;
+
+    }
+
+    private void FixedUpdate()
+    {
+                     
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 look = new Vector3(ray.origin.x, ray.origin.y, 0);
+          
+
+            Vector3 relativePos = look - transform.position;
+
+            float angle = Vector3.SignedAngle(relativePos, Vector3.down, Vector3.forward);
+            transform.rotation = Quaternion.Euler(0, 0, -angle);
+
+
+
+
+       
+
+
+
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigidBody.AddForce(Vector3.right * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigidBody.AddForce(Vector3.left * speed);
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigidBody.AddForce(Vector3.up * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rigidBody.AddForce(Vector3.down * speed);
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
